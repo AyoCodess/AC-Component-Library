4938949e4d6e232b0c897a4b8074e867ae98765b
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-07-27 11:54:09 +0200

    updated seo image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -46,7 +46,7 @@ export default async function IndexPage() {
             Follow me on github
           </Link>
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            Tailwind and Framer-motion Basic components
+            Tailwind & Framer-motion Base components
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             This is list of react tailwind custom components with animations I
