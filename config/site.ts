df18de96946d83578e006e5b501393be0c06627f
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-07-27 11:36:52 +0200

    updated landing page text

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "types"
 export const siteConfig: SiteConfig = {
   name: "AC Library",
   description:
-    "A list of custom react tailwind and framer-motion custom components I have created for various projects.",
+    "A list of custom react tailwind, framer-motion & Swipe.js custom components I have created for various projects.",
   url: "https://ayoadesanya.com",
   ogImage: "https://tx.shadcn.com/og.jpg",
   links: {
