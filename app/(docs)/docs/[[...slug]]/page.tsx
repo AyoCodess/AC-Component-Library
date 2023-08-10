f496b84ab4380780e57c0cc6c0bb0e21cb913b65
ayocodess <ayo.daniel.adesanya@gmail.com>
2023-08-10 08:58:35 +0200

    updated url links for components

--- app/(docs)/docs/[[...slug]]/page.tsx
+++ app/(docs)/docs/[[...slug]]/page.tsx
@@ -51,10 +51,10 @@ export async function generateMetadata({
   ogUrl.searchParams.set("mode", "dark")
 
   return {
-    title: doc.title,
+    title: doc.docTitle,
     description: doc.description,
     openGraph: {
-      title: doc.title,
+      title: doc.docTitle,
       description: doc.description,
       type: "article",
       url: absoluteUrl(doc.slug),
@@ -63,13 +63,13 @@ export async function generateMetadata({
           url: ogUrl.toString(),
           width: 1200,
           height: 630,
-          alt: doc.title,
+          alt: doc.docTitle,
         },
       ],
     },
     twitter: {
       card: "summary_large_image",
-      title: doc.title,
+      title: doc.docTitle,
       description: doc.description,
       images: [ogUrl.toString()],
     },
@@ -96,7 +96,7 @@ export default async function DocPage({ params }: DocPageProps) {
   return (
     <main className="relative py-6 lg:gap-10 lg:py-10 xl:grid xl:grid-cols-[1fr_100px]">
       <div className="mx-auto w-full min-w-0">
-        <DocsPageHeader heading={doc.title} text={doc.description} />
+        <DocsPageHeader heading={doc.docTitle} text={doc.description} />
         <Mdx code={doc.body.code} />
         {doc.title !== "Documentation" ? (
           <div>
